a0c64d2ea600de43cf256e3b729e575fc2277a70
Mamadou DIA <ibntalla@gmail.com>
2020-09-10 16:59:01 +0200

    firts

---
+++ precache-manifest.f3a9b2fbbb17eda7021116e2958d9ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b13c6fc4dee2e49bd9",
+    "url": "/css/app.5c3d074f.css"
+  },
+  {
+    "revision": "31eaa99b10f775a75333fe91984c1b9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b13c6fc4dee2e49bd9",
+    "url": "/js/app.1361595e.js"
+  },
+  {
+    "revision": "3e8f8472223cd18c20b7",
+    "url": "/js/chunk-vendors.aea18c08.js"
+  },
+  {
+    "revision": "0bd7541e17f600cad8478e1e63f89fce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
